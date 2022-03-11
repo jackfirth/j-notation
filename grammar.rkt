ca20677167fe96572bfc41a261ef9ca581246f0c
Jack Firth <jackhfirth@gmail.com>
2022-03-11 11:04:16 -0800

    Tweak grammar to make parse trees more readable

--- grammar.rkt
+++ grammar.rkt
@@ -1,23 +1,23 @@
 #lang brag
 
 program: statement*
-statement:
-  declaration ";"
-  | inline-definition ";"
-  | reassignment ";"
-  | inline-expression ";"
+@statement:
+  declaration /";"
+  | inline-definition /";"
+  | reassignment /";"
+  | inline-expression /";"
   | block-definition
   | block-expression
-block: "{" statement* "}"
+block: /"{" statement* /"}"
 
 # NAMES
 
-name: unindexed-name index-selection*
-unindexed-name: qualifier* SIMPLE-NAME
+name: @unindexed-name index-selection*
+unindexed-name: @qualifier* SIMPLE-NAME
 unqualified-name: SIMPLE-NAME index-selection*
-qualifier: unqualified-name "."
-index-selection: "[" [expression ("," expression)* [","]] "]"
-operator-name: qualifier* SIMPLE-OPERATOR-NAME
+qualifier: @unqualified-name /"."
+index-selection: /"[" [expression (/"," expression)* [/","]] /"]"
+operator-name: @qualifier* SIMPLE-OPERATOR-NAME
 
 # DECLARATIONS
 
@@ -32,42 +32,43 @@ operator-name: qualifier* SIMPLE-OPERATOR-NAME
 #
 #   #[deprecated] mutable val x
 #
-declaration: attribute* name+ header annotation*
+declaration: attribute* modifier* kind header annotation*
+modifier: name
+kind: name
 
-
-contextual-declaration: attribute* name* header annotation*
-header: unindexed-name indices-specification* [components-specification]
-components-specification: "(" [contextual-specification ("," contextual-specification)* [","]] ")"
-indices-specification: "[" [contextual-specification ("," contextual-specification)* [","]] "]"
-annotation: ":" expression
-attribute: "#" "[" expression "]"
+contextual-declaration: attribute* modifier* header annotation*
+@header: unindexed-name indices-specification* [components-specification]
+components-specification:
+  /"(" [contextual-specification (/"," contextual-specification)* [/","]] /")"
+indices-specification: /"[" [contextual-specification (/"," contextual-specification)* [/","]] /"]"
+annotation: /":" expression
+attribute: /"#" /"[" expression /"]"
 
 # STATEMENTS
 
-assignment: "=" expression
+/assignment: /"=" expression
 reassignment: name assignment
-specification: declaration | definition
-contextual-specification: contextual-declaration | contextual-definition
-inline-specification: declaration | inline-definition
-definition: inline-definition | block-definition
+@specification: declaration | definition
+@contextual-specification: contextual-declaration | contextual-definition
+@inline-specification: declaration | inline-definition
+@definition: inline-definition | block-definition
 contextual-definition: contextual-declaration (assignment | block)
-inline-definition: declaration assignment
-block-definition: declaration block
+inline-definition: @declaration assignment
+block-definition: @declaration block
 
 # EXPRESSIONS
 
-expression: inline-expression | block-expression
-inline-expression:
-  name | literal | inline-invocation | inline-operation | "(" expression ")"
+expression: @inline-expression | @block-expression
+@inline-expression:
+  name | literal | inline-invocation | inline-operation | /"(" expression /")"
 
-block-expression:
-  block-invocation
-  | block-operation
+@block-expression: block-invocation | block-operation
 
 inline-invocation:
-  [inline-expression "."] unqualified-name "(" [(expression | reassignment) ("," (expression | reassignment))* [","]] ")"
+  [inline-expression /"."] unqualified-name
+  /"(" [(expression | reassignment) (/"," (expression | reassignment))* [/","]] /")"
 block-invocation:
-  inline-invocation block
+  @inline-invocation block
 
 inline-operation:
   inline-expression operator-name inline-expression
