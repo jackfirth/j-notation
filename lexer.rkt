b146f2eb8122e70eac233f4a629abe01e42a81e7
Jack Firth <jackhfirth@gmail.com>
2025-05-24 00:29:09 -0700

    Simplify tokenizer definition

--- lexer.rkt
+++ lexer.rkt
@@ -26,25 +26,25 @@
 (define-lex-abbrev operator (:+ (char-set "#.!=<>+-/*^%:")))
 
 
-(define (make-tokenizer port)
-  (define (next-token)
-    (define bf-lexer
-      (lexer-srcloc
-       [line-comment (token 'LINE-COMMENT #:skip? #true)]
-       [(:+ whitespace) (token 'WHITESPACE #:skip? #true)]
-       [";" (token 'SEMICOLON)]
-       ["," (token 'COMMA)]
-       ["[" (token 'LEFT-SQUARE-BRACKET)]
-       ["]" (token 'RIGHT-SQUARE-BRACKET)]
-       ["(" (token 'LEFT-PARENTHESIS)]
-       [")" (token 'RIGHT-PARENTHESIS)]
-       ["{" (token 'LEFT-CURLY-BRACKET)]
-       ["}" (token 'RIGHT-CURLY-BRACKET)]
-       [(:seq "}" (:* whitespace-excluding-newline) "\n") (token 'RIGHT-CURLY-BRACKET-AND-NEWLINE)]
-       [name (token 'NAME (string->symbol lexeme))]
-       [operator (token 'OPERATOR (string->symbol lexeme))]
-       [literal-integer (token 'LITERAL-INTEGER (string->number lexeme))]
-       [literal-decimal (token 'LITERAL-DECIMAL (string->number lexeme))]
-       [literal-string (token 'LITERAL-STRING (substring lexeme 1 (sub1 (string-length lexeme))))]))
-    (bf-lexer port))
-  next-token)
+(define j-notation-lexer
+  (lexer-srcloc
+   [line-comment (token 'LINE-COMMENT #:skip? #true)]
+   [(:+ whitespace) (token 'WHITESPACE #:skip? #true)]
+   [";" (token 'SEMICOLON)]
+   ["," (token 'COMMA)]
+   ["[" (token 'LEFT-SQUARE-BRACKET)]
+   ["]" (token 'RIGHT-SQUARE-BRACKET)]
+   ["(" (token 'LEFT-PARENTHESIS)]
+   [")" (token 'RIGHT-PARENTHESIS)]
+   ["{" (token 'LEFT-CURLY-BRACKET)]
+   ["}" (token 'RIGHT-CURLY-BRACKET)]
+   [(:seq "}" (:* whitespace-excluding-newline) "\n") (token 'RIGHT-CURLY-BRACKET-AND-NEWLINE)]
+   [name (token 'NAME (string->symbol lexeme))]
+   [operator (token 'OPERATOR (string->symbol lexeme))]
+   [literal-integer (token 'LITERAL-INTEGER (string->number lexeme))]
+   [literal-decimal (token 'LITERAL-DECIMAL (string->number lexeme))]
+   [literal-string (token 'LITERAL-STRING (substring lexeme 1 (sub1 (string-length lexeme))))]))
+
+
+(define ((make-tokenizer port))
+  (j-notation-lexer port))
