a273464204f612eeaf8a866601dd8bdcf5f9c9eb
Jack Firth <jackhfirth@gmail.com>
2025-05-24 02:15:59 -0700

    Implement lexer chaining

--- lexer.rkt
+++ lexer.rkt
@@ -31,34 +31,66 @@
 (define-lex-abbrev operator (:+ (char-set "#.!=<>+-/*^%:")))
 
 
+(struct lexer-result (token next-mode) #:transparent)
+
+
 (define j-notation-lexer
-  (lexer-srcloc
-   [line-comment (token 'LINE-COMMENT #:skip? #true)]
-   [(:seq (:* whitespace-excluding-newline) "\n") (token 'WHITESPACE #:skip? #true)]
-   [(:+ whitespace-excluding-newline) (token 'WHITESPACE #:skip? #true)]
-   [";" (token 'SEMICOLON)]
-   ["," (token 'COMMA)]
-   ["[" (token 'LEFT-SQUARE-BRACKET)]
-   ["]" (token 'RIGHT-SQUARE-BRACKET)]
-   ["(" (token 'LEFT-PARENTHESIS)]
-   [")" (token 'RIGHT-PARENTHESIS)]
-   ["{" (token 'LEFT-CURLY-BRACKET)]
-   ["}" (token 'RIGHT-CURLY-BRACKET)]
-   [(:seq "}" (:* whitespace-excluding-newline) "\n") (token 'RIGHT-CURLY-BRACKET-AND-NEWLINE)]
-   [name (token 'NAME (string->symbol lexeme))]
-   [operator (token 'OPERATOR (string->symbol lexeme))]
-   [literal-integer (token 'LITERAL-INTEGER (string->number lexeme))]
-   [literal-decimal (token 'LITERAL-DECIMAL (string->number lexeme))]
+  (lexer
+   [line-comment
+    (lexer-result (make-srcloc-token (token 'LINE-COMMENT #:skip? #true) lexeme-srcloc)
+                  j-notation-lexer)]
+   [(:seq (:* whitespace-excluding-newline) "\n")
+    (lexer-result (make-srcloc-token (token 'WHITESPACE #:skip? #true) lexeme-srcloc)
+                  j-notation-lexer)]
+   [(:+ whitespace-excluding-newline)
+    (lexer-result (make-srcloc-token (token 'WHITESPACE #:skip? #true) lexeme-srcloc)
+                  j-notation-lexer)]
+   [";" (lexer-result (make-srcloc-token (token 'SEMICOLON) lexeme-srcloc) j-notation-lexer)]
+   ["," (lexer-result (make-srcloc-token (token 'COMMA) lexeme-srcloc) j-notation-lexer)]
+   ["["
+    (lexer-result (make-srcloc-token (token 'LEFT-SQUARE-BRACKET) lexeme-srcloc) j-notation-lexer)]
+   ["]"
+    (lexer-result (make-srcloc-token (token 'RIGHT-SQUARE-BRACKET) lexeme-srcloc) j-notation-lexer)]
+   ["(" (lexer-result (make-srcloc-token (token 'LEFT-PARENTHESIS) lexeme-srcloc) j-notation-lexer)]
+   [")" (lexer-result (make-srcloc-token (token 'RIGHT-PARENTHESIS) lexeme-srcloc) j-notation-lexer)]
+   ["{" (lexer-result (make-srcloc-token (token 'LEFT-CURLY-BRACKET) lexeme-srcloc) j-notation-lexer)]
+   ["}"
+    (lexer-result (make-srcloc-token (token 'RIGHT-CURLY-BRACKET) lexeme-srcloc) j-notation-lexer)]
+   [(:seq "}" (:* whitespace-excluding-newline) "\n")
+    (lexer-result (make-srcloc-token (token 'RIGHT-CURLY-BRACKET-AND-NEWLINE) lexeme-srcloc)
+                  j-notation-lexer)]
+   [name
+    (lexer-result (make-srcloc-token (token 'NAME (string->symbol lexeme)) lexeme-srcloc)
+                  j-notation-lexer)]
+   [operator
+    (lexer-result (make-srcloc-token (token 'OPERATOR (string->symbol lexeme)) lexeme-srcloc)
+                  j-notation-lexer)]
+   [literal-integer
+    (lexer-result (make-srcloc-token (token 'LITERAL-INTEGER (string->number lexeme)) lexeme-srcloc)
+                  j-notation-lexer)]
+   [literal-decimal
+    (lexer-result (make-srcloc-token (token 'LITERAL-DECIMAL (string->number lexeme)) lexeme-srcloc)
+                  j-notation-lexer)]
    [literal-single-line-string
-    (token 'LITERAL-STRING (substring lexeme 1 (sub1 (string-length lexeme))))]
+    (let ()
+      (define raw-token (token 'LITERAL-STRING (substring lexeme 1 (sub1 (string-length lexeme)))))
+      (lexer-result (make-srcloc-token raw-token lexeme-srcloc) j-notation-lexer))]
    [literal-multiline-string
     (let ()
       (define indentation (position-col start-pos))
       (define indented-string (substring lexeme 3 (- (string-length lexeme) 3)))
       (define without-indentation
         (string-replace indented-string (string-append "\n" (make-string indentation #\space)) "\n"))
-      (token 'LITERAL-STRING without-indentation))]))
+      (define raw-token (token 'LITERAL-STRING without-indentation))
+      (lexer-result (make-srcloc-token raw-token lexeme-srcloc) j-notation-lexer))]))
 
 
-(define ((make-tokenizer port))
-  (j-notation-lexer port))
+(define (make-tokenizer port)
+  (define lexer j-notation-lexer)
+  (λ ()
+    (define result (lexer port))
+    (cond
+      [(eof-object? result) result]
+      [else
+       (set! lexer (lexer-result-next-mode result))
+       (lexer-result-token result)])))
