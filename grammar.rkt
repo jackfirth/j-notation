d66eeedccf7d82b652baab35d531f9793d0cc7a3
Jack Firth <jackhfirth@gmail.com>
2025-06-07 00:01:55 -0700

    Add some grammar comments
    Maybe someday these can be machine-checkable.

--- grammar.rkt
+++ grammar.rkt
@@ -1,7 +1,32 @@
 #lang brag
 
+
+# no left recursion
+
+
 @body: () | /SEMICOLON | inline-group | end-group | inline-group /SEMICOLON body | end-group body
+  # never includes: inline-group inline-group
+  # can include: inline-group SEMICOLON group
+  # can include: end-group group
+  # never ends with: inline-group
+  # can start with: group
+  # can end with: end-group
+
+
+# for all following rules:
+#   never includes: group group
+#   never includes: body body
+
+
 @group-list: () | /COMMA | group | group /COMMA group-list
+
+
+# for all following rules:
+#   never matches: ()
+#   no undelimited recursion
+#   always starts with: atom | left-delimiter
+#   always ends with: atom | right-delimiter
+
 
 @group: inline-group | end-group
 inline-group: inline-expression+
@@ -17,3 +42,14 @@ end-block: /LEFT-CURLY-BRACKET body /RIGHT-CURLY-BRACKET-AND-NEWLINE
 
 string-template:
   LEFT-STRING-TEMPLATE group (MIDDLE-STRING-TEMPLATE group)* RIGHT-STRING-TEMPLATE
+
+
+left-delimiter: LEFT-PARENTHESIS
+              | LEFT-SQUARE-BRACKET
+              | LEFT-CURLY-BRACKET
+              | LEFT-STRING-TEMPLATE
+
+right-delimiter: RIGHT-PARENTHESIS
+               | RIGHT-SQUARE-BRACKET
+               | RIGHT-CURLY-BRACKET
+               | RIGHT-STRING-TEMPLATE
